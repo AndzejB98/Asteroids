9eb5d746c8b7c43c8d2f4588b39cbf453715280f
andzej_b <trane59@gmail.com>
2025-07-24 13:25:56 +0300

    added asteroids and made them move randomly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame # type: ignore
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 pygame.init()
 
@@ -9,10 +11,15 @@ dt = 0
 
 updatable = pygame.sprite.Group()
 drawable = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
 
 Player.containers = (updatable, drawable)
+Asteroid.containers = (asteroids, updatable, drawable)
+AsteroidField.containers = (updatable)
 
 player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
+asteroidfield = AsteroidField()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
